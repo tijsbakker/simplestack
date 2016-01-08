15c69bca1921de5e030f45b0bfc9a4c2ab31fc1d
tijsbakker <tijs@goddog.nl>
2016-01-08 13:28:14 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,5 +5,5 @@ export DEBIAN_FRONTEND=noninteractive
 aptitude -y install python-m2crypto
 
 echo "Creating symbolic links for salt root and pillar folder"
-ln -s /home/ubuntu/sites/simplestack/salt/roots/ /srv/salt
-ln -s /home/ubuntu/sites/simplestack/salt/pillar/ /srv/pillar
+ln -s /home/goddog/sites/simplestack/salt/roots/ /srv/salt
+ln -s /home/goddog/sites/simplestack/salt/pillar/ /srv/pillar
